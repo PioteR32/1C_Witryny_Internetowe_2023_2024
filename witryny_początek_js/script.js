3d707946aa565bfe7a13bfe7b3d575041b9c8058
Uczen <Uczen@214-17>
2024-09-10 15:05:01 +0200

    2lekcja

--- witryny_początek_js/script.js
+++ witryny_początek_js/script.js
@@ -1,1 +1,6 @@
-document.write("coś tam")
+document.write("plik js<br>")
+document.write("Imię Nazwisko <br>");
+document.write("Piotr Synowiec <br>");
+document.write("Wiek : 15");
+
+console.log("Imię Nazwisko");
